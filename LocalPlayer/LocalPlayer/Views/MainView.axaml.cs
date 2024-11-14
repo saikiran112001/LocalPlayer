e02ed0e0b8146ab03aed4034d4c0e13f97c13193
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-11-14 16:48:14 +0530

    Add project files.

---
+++ LocalPlayer/LocalPlayer/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LocalPlayer.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LocalPlayer/LocalPlayer/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LocalPlayer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
